5e156292bd04bbbb93c36142d5a84c6ad5b7d525
bolvar <123@edu.cn>
2024-02-05 02:55:30 +0800

    新增登录认证

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as dj_login
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
 
 # Create your views here.
+def index(request):
+    pass
+
+
+def login(request):
+    say_alter = {'go': True}
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            logging.debug("登录form验证成功!\t用户名:{0}\t密码:{1}".format(username, password))
+            user = authenticate(username=username, password=password)
+            if user.is_active:
+                dj_login(request, user)
+                logging.debug("账户已启用")
+                request.session.set_expiry(0)
+                return redirect("/index")
+            else:
+                logging.debug("账户未启用,发送弹窗")
+                return render(request, "login.html", {"form": form, "say_alter": say_alter})
+        else:
+            logging.debug("登录form验证失败")
+            logging.debug("正在进入GET请求视图")
+    return render(request, "login.html", {"form": form})
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for jangosite project.
+URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views as app1_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', app1_views.login)
 ]
